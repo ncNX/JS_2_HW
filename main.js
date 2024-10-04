63a82ac21e1c127f6b3caf661714cfa68621bc6c
Igor <dumanschi.igor@gmail.com>
2024-10-04 12:22:59 +0300

    JS_2_HW

---
+++ main.js
@@ -0,0 +1,42 @@
+// const slots = 100;
+
+// if (slots === 100) {
+//   console.log("nu mai sunt locuri");
+// } else {
+//   console.log("Locuri sunt");
+// }
+
+// slots === 100 ? console.log('nu mai sunt locuri ') : console.log('Locuri sunt')
+
+// const condition1 = 0;
+// const condition2 = 1;
+
+// const statut = 1
+
+// if ( statut !== condition1) {
+// 	console.log('ocupat')
+// } else {
+// 	console.log('liber')
+// }
+
+// statut !== condition2 ? console.log('liber') : console.log('ocupat')
+
+// stockStatus = 120;
+
+// if (stockStatus >= 100) {
+//   console.log("overloaded");
+// } else if (stockStatus === 100) {
+//   console.log("full");
+// } else if (stockStatus >= 50) {
+//   console.log("medium");
+// } else {
+//   console.log("low");
+// }
+
+// stockStatus >= 100
+//   ? console.log("overloaded")
+//   : stockStatus === 100
+//   ? console.log("full")
+//   : stockStatus >= 50
+//   ? console.log("medium")
+//   : console.log("low");
